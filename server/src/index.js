9f0157bb8f2bcfaf23d382d1d50682b10a44ee3d
apeshhev <atanas.peshev8@abv.bg>
2021-06-08 21:00:29 +0300

    Updated server part

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var app = express();
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
-const url = "mongodb://127.0.0.1:27017";
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://Team17:Team17@team17.z8081.mongodb.net/Team17?retryWrites=true&w=majority'
+
+var connectDB = () => {
+    return mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+};
 
 app.get('/', function (req, res) {
     res.send('Hello Team17!');
 });
 
-MongoClient.connect(url, function(err, client) {
-    if(err) {
-        client.close();
-        return;
-    }
-
-    console.log('DB is listening on ' + url);
-
+connectDB()
+.then(() => {
+    console.log('Database connection successful');
     app.listen(3001, function () {
         console.log("Server is listening on port 3001");
     });
-});
+})
+.catch(error => console.error('Database connection error'));
+
+
 
